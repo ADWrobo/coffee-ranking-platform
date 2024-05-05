caa955a656d59b102698e9cb80a59ce666768e99
Elizabeth Rowley <Elizabeth.Rowley@bsci.com>
2024-05-04 23:38:29 -0500

    GET Request working fullstack

--- angular-frontend/src/app/card/card.component.ts
+++ angular-frontend/src/app/card/card.component.ts
@@ -1,17 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IRank } from '../models/IRank';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
 
-  drinkTitle="Drink Name";
-  cafeName="Cafe";
-  location="Location";
-  rating="★★";
-  ratingRemainder="★★★"; //This is the difference between the rating and 5 stars
-  comments="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pellentesque purus id magna molestie mollis. Aliquam orci neque, laoreet condimentum varius quis, blandit vitae nunc. Vivamus in ultrices nibh. Fusce sit amet luctus felis, sed egestas orci. Nam porta dui mi, non luctus tellus malesuada sit amet. Nullam sed varius mauris, vel pretium arcu. Vestibulum arcu nisl, faucibus in erat a, blandit gravida mauris. Fusce et erat vel lectus gravida tristique. Quisque consectetur posuere tellus, a faucibus tortor pretium in. Maecenas eleifend nunc et justo suscipit, ac egestas massa egestas.";
+  ranks: IRank[] = [];
+
+  constructor(public httpClient: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.getRanks();
+  }
+
+  getRanks() {
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      })
+    };
+
+    this.httpClient.get<IRank[]>("http://localhost:8080/ranks", options)
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+          this.ranks = data;
+        },
+        error: (err) => {
+          console.error("Error occurred: " + err);
+        }
+      });
+  }
 
 }
--- angular-frontend/src/app/models/IRank.ts
+++ angular-frontend/src/app/models/IRank.ts
@@ -1,8 +1,8 @@
 export interface IRank {
-    drinkTitle: string;
-    cafeName: string;
+    drink: string;
+    cafe: string;
     location: string;
     rating: string;
     ratingRemainder: string; //This is the difference between the rating and 5 stars
-    comments: string;
+    comment: string;
 }
