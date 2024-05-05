b85b890620d64cad4089d761801dbbc0e02fcc86
Elizabeth Rowley <Elizabeth.Rowley@bsci.com>
2024-05-04 22:45:56 -0500

    Backend Get Request finished

--- express-backend/src/index.ts
+++ express-backend/src/index.ts
@@ -6,6 +6,11 @@ const fs = require("fs");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
+//Use express to listen to port
+app.listen(port, () => {
+  console.log(`Express server is running on port ${port}`);
+});
+
 //configure cors
 const cors = require("cors");
 let corsOptions = {
@@ -19,30 +24,21 @@ app.use(express.json());
 //Read json file
 const jsonPath = path.join(__dirname, "coffee-drinks.json");
 
-//import { IRank } from './src/app/models/IRank';
+import { IRank } from '../../angular-frontend/src/app/models/IRank';
 
-//let ranks: IRank[] = [];
+let ranks: IRank[] = [];
 
 //parse out ranks
-// fs.readFile(jsonPath, function (err: any, data: any) {
-//   if (err) {
-//     console.log('Unable to read json data file: ' + jsonPath);
-//   } else {
-//     ranks = JSON.parse(data);
-//   }
-// });
-
-//Use express to listen to port
-app.listen(port, () => {
-  console.log(`Express server is running on port ${port}`);
+fs.readFile(jsonPath, function (err: any, data: any) {
+  if (err) {
+    console.log('Unable to read json data file: ' + jsonPath);
+  } else {
+    ranks = JSON.parse(data);
+  }
 });
 
 // GET endpoint returning all ranks
-// app.get('/ranks', function(req: any, res: any) {
-//   res.status(200);
-//   return res.json(ranks);
-// });
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'src', 'index.html'));
-// });
+app.get('/ranks', function(req: any, res: any) {
+  res.status(200);
+  return res.json(ranks);
+});
