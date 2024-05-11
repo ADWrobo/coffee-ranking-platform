07e7a97708831768c0c759e894adff6ace62d2e1
Charles Allhands <cra@resistentialist.com>
2024-05-10 20:45:12 -0500

    Add delete route

--- express-backend/src/index.ts
+++ express-backend/src/index.ts
@@ -32,6 +32,7 @@ let ranks: IRank[] = [];
 fs.readFile(jsonPath, function (err: any, data: any) {
   if (err) {
     console.log('Unable to read json data file: ' + jsonPath);
+    console.log(jsonPath);
   } else {
     ranks = JSON.parse(data);
   }
@@ -41,4 +42,24 @@ fs.readFile(jsonPath, function (err: any, data: any) {
 app.get('/ranks', function(req: any, res: any) {
   res.status(200);
   return res.json(ranks);
+});
+
+// DELETE endpoint
+app.delete("/ranks/:id", (req: any, res: any) => {
+  if (req.params.id) {
+    const rank = ranks.find((c) => c.id == req.params.id);
+    if (rank) {
+      delete rank.id;
+      delete rank.drink;
+      delete rank.cafe;
+      delete rank.location;
+      delete rank.rating;
+      delete rank.ratingRemainder;
+      delete rank.comment;
+      res.status(200);
+      return res.json(ranks);
+    }
+  }
+  res.status(404);
+  return res.json({ error: `Drink with id: ${req.params.id} not found.` });
 });
