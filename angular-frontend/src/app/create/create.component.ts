1bbb00547474e32039324b53d576ddf63e91ece7
Elizabeth Rowley <Elizabeth.Rowley@bsci.com>
2024-05-11 10:16:28 -0500

    Adds form

--- angular-frontend/src/app/create/create.component.ts
+++ angular-frontend/src/app/create/create.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { IRank } from '../models/IRank';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
@@ -6,5 +10,63 @@ import { Component } from '@angular/core';
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+
+  rank: IRank;
+  id: number;
+  drink: string;
+  cafe: string;
+  location: string;
+  rating: string;
+  ratingRemainder: string;
+  comment: string;
+  saveUnsuccessful: boolean;
+  httpClient: any;
+
+ constructor(httpClient: HttpClient, public router: Router) {
+  
+ }
+
+  onFormSubmit(ngForm: NgForm) {
+
+    this.saveUnsuccessful = false;
+
+    if (!ngForm.valid) {
+      this.saveUnsuccessful = true;
+      alert("All Fields are Required");
+      return;
+    }
+    console.log(ngForm);
+
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        "Accept": 'application/json'
+      })
+    };
+
+    const data = {
+      id: this.id,
+      drink: this.drink,
+      cafe: this.cafe,
+      location: this.location,
+      rating: this.rating,
+      ratingRemainder: this.ratingRemainder,
+      comment: this.comment
+    };
+
+    this.httpClient.post("http://localhost:8080/ranks", data, options)
+      .subscribe({
+        next: () => {
+          console.log("Call successful");
+        },
+        error: (err) => {
+          console.error("Error occured: " + JSON.stringify(err));
+          alert("A rank with this id already exists.")
+        }
+      });
+
+    ngForm.resetForm();
+    this.router.navigate(['home']);
+  }
 
 }
